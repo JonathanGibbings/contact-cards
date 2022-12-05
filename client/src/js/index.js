ffa9d48a090ab8dce61915bcf18ddd134b3e4758
JGibbings <j_gibbings@msn.com>
2022-12-04 21:49:16 -0500

    add get and post to db

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -7,10 +7,15 @@ import Logo from "../images/logo.png";
 import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
 
-import { initDb } from "./database";
+import { initDb, getDb, postDb } from "./database";
 
 window.addEventListener("load", function () {
   initDb();
+
+  getDb();
+  postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
+  getDb();
+
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;

1c7b429a045e08883bbb290296f226d562f5e897
JGibbings <j_gibbings@msn.com>
2022-12-04 21:35:09 -0500

    initdb initialized

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,18 +1,20 @@
-// Import modules
 import "./form";
 import "./submit";
-
-// Import CSS files
-import "../css/index.css";
 import { Tooltip, Toast, Popover } from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// import images
 import Logo from "../images/logo.png";
 import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
+
+import { initDb } from "./database";
+
 window.addEventListener("load", function () {
+  initDb();
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;
 });
+
+// Import CSS files
+import "../css/index.css";
